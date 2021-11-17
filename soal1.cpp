72df52b4f1bccc647277194e49f8ec59ac574d18
root <root@anonymous.localdomain>
2021-11-17 18:44:13 +0700

    first commit

---
+++ soal1.cpp
@@ -0,0 +1,20 @@
+//riyan saputra
+//20200801081
+//uts bahsa pemograman
+
+//Preprocessing 
+#include <iostream> //digunakan sebagai standar input output operasi
+
+using namespace std; // digunakan untuk memanggil namespace yang memiliki nama 'std'
+//"std" ruang nama. Operator "::" 
+//adalah operator "lingkup". Ini memberi tahu kompiler kelas/ruang nama mana yang harus dicari untuk pengidentifikasi.
+
+int main(){ // merupakan fungsi utama yang akan dibaca oleh kompilator
+    int r ; // memasukan int r
+        cout<<" Masukan paswordnya \n"<<endl; // menampilkan hasil output
+        cin>>r; //memanggil integer yang sudah di tetapkan
+        cout<<"\n"; // menambah space line 
+        cout<<" Password Berhasil "<<endl; //menampilkan hasil  output
+
+    return 0; // Menyatakan hasil keluaran dari fungsi main() adalah 0
+}
