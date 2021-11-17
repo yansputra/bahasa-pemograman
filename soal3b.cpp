27e5c6213aab975899daf428995d42db455fe004
root <root@anonymous.localdomain>
2021-11-17 19:38:53 +0700

    first commit

---
+++ soal3b.cpp
@@ -0,0 +1,29 @@
+//riyan saputra 20200801081
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main(){
+    int y,i,z,x;
+    int q;
+        cout<<"masukkan Jumlah Maks Bintang = "; cin>>i;cout<<endl;
+            z=i;
+    for(x=1;x<=i;x++){ 
+{
+    for (y=1;y<=x;y++){
+        cout<<" ";
+    }
+}
+ 
+{
+    q=1;
+    for (q=1;q<=z;q++){
+        cout<<"*";
+    } 
+}
+        cout<<endl;
+    z--;
+    }
+
+}
